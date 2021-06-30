99c5893967105b76d95b1ff00dcc57628a4a119d
SergeyLepeshinsky <thsname18@gmail.com>
2021-06-30 18:31:52 +0300

    добавил класс как пример инкапсуляции

---
+++ Lesson10incapsulation/Incapsulation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson10incapsulation
+{
+    /// <summary>
+    /// Перевод числа в строку
+    /// </summary>
+    class IncapsulationExample
+    {
+        private double doubleValue;
+        private string stringValue;
+
+        public double DoubleValue
+        {
+            get { return doubleValue; }
+            set
+            {
+                doubleValue = value;
+                stringValue = value.ToString();
+            }
+        }
+        public string StringValue
+        {
+            get { return stringValue; }
+            set
+            {
+                stringValue = value;
+                try
+                {
+                    double doubleTemp = Convert.ToDouble(stringValue);
+                    doubleValue = doubleTemp;
+                }
+                catch (Exception)
+                {
+                    
+
+                }
+                
+            }
+        }
+        public void Sum()
+        {
+            Console.WriteLine($"Число: {doubleValue}");
+            Console.WriteLine($"Строка: {stringValue}");
+            Console.WriteLine($"Число + строка: {doubleValue + stringValue}");
+        }
+
+    }
+}
---
+++ Lesson10incapsulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson10incapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var incapsulation = new IncapsulationExample();
+            Console.WriteLine(incapsulation.GetType());
+
+            incapsulation.DoubleValue = 0.0001;
+            string s = incapsulation.StringValue;
+            Console.WriteLine(s);
+
+            incapsulation.Sum();
+
+            incapsulation.StringValue = "qwert";
+
+            incapsulation.Sum();
+
+
+
+        }
+    }
+}
